2d23f6a9b5df35ec98226bf02d694e933255cfd9
Morshaedul <morshaedulanirbanit@gmail.com>
2022-09-15 23:57:11 +0600

    first commit

---
+++ Js/index.js
@@ -0,0 +1,21 @@
+
+let timeout;
+
+function myFunction() {
+  timeout = setTimeout(alertFunc, 3000);
+}
+
+function alertFunc() {
+  alert("Hello!");
+}
+
+
+
+
+
+
+// var date =new Date();
+// var currentDate=date.getFullYear();
+// var Date=date.getDay();
+// document.write(currentDate);
+// document.write(date);
